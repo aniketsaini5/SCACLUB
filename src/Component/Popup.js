01daa4a11d50b0b0177ebb264d2312c4d78b16d2
Aniket Saini <154746704+aniketsaini5@users.noreply.github.com>
2024-09-02 20:57:15 +0530

    add event

--- src/Component/Popup.js
+++ src/Component/Popup.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import './Popup.css';
 import imge from './placement-img/image.png';
+import image from './resource/img.jpeg';
+import pdf from './resource/Rule Book.pdf';
 
 const Popup = ({ type, onClose }) => {
     useEffect(() => {
@@ -18,22 +20,42 @@ const Popup = ({ type, onClose }) => {
             case 'events':
                 return (
                     <>
-                        <h2 className="popup-title">Upcoming Events</h2>
+                        <h2 className="popup-title">Event</h2>
+                        <h3 className="popup-subtitle-event">
+                            <span className="popup-title-left">Engineering Day</span>
+                            <span className="popup-title-right">
+                                <i><small className="Date">14 September 2024</small></i>
+                            </span>
+                        </h3>
+
+                        <img src={image} alt="engineering day" className="popup-image" />
+
+                        <div className="popup-links">
+                            <a href="https://forms.gle/7jVuNSh9GCEivoAA9" target="_blank" rel="noopener noreferrer" className="popup-link">
+                                Register Now
+                            </a>
+                            <a href={pdf} target="_blank" rel="noopener noreferrer" className="popup-link" id="rulebook" download={pdf}>
+                                Download Rulebook
+                            </a>
+                        </div>
+
+
+                        {/* <h2 className="popup-title">Upcoming Events</h2>
                         <h3 className="popup-subtitle">Engineering Day</h3>
-                        <p className="popup-description"><i>-update soon</i></p>
+                        <p className="popup-description"><i>-update soon</i></p> */}
                     </>
                 );
             case 'hackathons':
                 return (
                     <>
-                        {/* <h2 className="popup-title">Hackathon</h2>
-                        <img src="https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/banner/66bf9bfd9347a_code-red.webp?d=1920x557" alt="Hackathon" className="popup-image" />
-                        <h3 className="popup-subtitle">Code Red</h3>
-                        <p className="popup-description">Click on More Details for more information about the hackathon, including registration and prizes.</p>
-                        <a href="https://unstop.com/hackathons/code-red-nitk-surathkal-1126425" target="_blank" rel="noopener noreferrer" className="popup-link">More Details</a> */}
-
                         <h2 className="popup-title">Hackathon</h2>
-                        <h3 className="popup-subtitle">Upcoming</h3>
+                        <img src="https://d8it4huxumps7.cloudfront.net/uploads/images/150x150/uploadedManual-66ab7658502ef_listing_image_ml_challenge__1_.jpg?d=200x2007" alt="Hackathon" className="popup-image" />
+                        <h3 className="popup-subtitle">Amazon ML</h3>
+                        <p className="popup-description">Click on More Details for more information about the hackathon, including registration and prizes.</p>
+                        <a href="https://unstop.com/hackathons/amazon-ml-challenge-amazon-1100713" target="_blank" rel="noopener noreferrer" className="popup-link">More Details</a>
+
+                        {/* <h2 className="popup-title">Hackathon</h2>
+                        <h3 className="popup-subtitle">Upcoming</h3> */}
                     </>
                 );
             case 'placement':
